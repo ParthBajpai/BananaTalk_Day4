15fe347def60949f6f837d02af73f2a8880bdfeb
ParthBajpai <parthbajpai82@gmail.com>
2021-10-01 14:03:57 +0530

    input output box created

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
-btnTranslate.addEventListener("click", function clickEventHandler(){
-    console.log("clicked!")
-})
+outputDiv.innerText = "parth bajpai"
+console.log(outputDiv)
+
+
+function clickHandler(){
+    console.log("clicked!");
+    console.log("input",txtInput.value)
+};
+
+
+btnTranslate.addEventListener("click",clickHandler)
